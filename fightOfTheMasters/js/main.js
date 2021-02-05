b8330131f25aa7cdb166453cc47e6d0952b799c3
Daniel Castillo <danielcastillog92@gmail.com>
2021-02-05 09:45:20 +0100

    75% del motor hecho. Selección de personajes y display en la siguiente fase hecho. Inicio de combate

--- fightOfTheMasters/js/main.js
+++ fightOfTheMasters/js/main.js
@@ -1,7 +1,15 @@
 //Botón de ataque responde
 
 let atk = () => {
-    alert("Está vivo!!!!");
+    let turn = Math.floor(Math.random() * 2);
+    let critHit = Math.floor(Math.random() * 5);
+
+    if (turn = 0)  {
+        if (critHit == 5) {
+            console.log("Critical Hit!!!");
+            player1.
+        }
+    }
 };
 
 let resetGame = () => {
@@ -19,7 +27,7 @@ class Leyenda {
         this.strength = strength;
         this.defense = defense;
         this.agility = agility;
-        this.stamina = agility * (strength / 1.5);
+        this.stamina = Math.floor(agility * (strength / 1.5));
     };
 
     ataque(enemy) {
@@ -38,28 +46,52 @@ let player2 = new Leyenda("samus", 1000, 250, 300, 300);
 let player3 = new Leyenda("mario", 1000, 200, 350, 200);
 let player4 = new Leyenda("megaman", 1000, 300, 200, 350);
 
-let select = (character) => {
+let select = (playerTag) => {
     //console.log("Has seleccionado a..." + character);
     if (p1 == "") {
-        p1 = character;
-
-        document.getElementById(character).className = "selected";
-        document.getElementById(character).onclick = "";
+        p1 = allPlayers[playerTag];
 
         let personaje1 = document.getElementById("personaje1");
         personaje1.innerHTML = `You have chosen ${p1.name}`;
     } else {
-        p2 = character;
+        p2 = allPlayers[playerTag];
 
-        document.getElementById(character).className = "selected";
-        document.getElementById(character).onclick = "";
+        let personaje2 = document.getElementById("personaje2");
 
-        let mensaje2 = document.getElementById("personaje2");
+        personaje2.innerHTML = `You have chosen ${p2.name}`;
 
-        mensaje2.innerHTML = `You have chosen ${p2.name}`;
+        fasingSwitch("chosingFase", "fightingFase");
+
+        let guerrero1 = document.getElementById("guerrero1");
+        let guerrero2 = document.getElementById("guerrero2");
+
+        guerrero1.innerHTML = `<div id="guerrero_${p1.name}" class="estiloGuerrero" >
+            <img  src="fightOfTheMasters/img/${p1.name}.jpg">
+            <div class="name">${p1.name}</div>
+            <div class="health">${p1.health}</div>
+            <div class="strength">${p1.strength}</div>
+            <div class="defense">${p1.defense}</div>
+            <div class="agility">${p1.agility}</div>
+            <div class="stamina">${p1.stamina}</div>
+
+        </div>`;
+        guerrero2.innerHTML = `<div id="guerrero_${p2.name}" class="estiloGuerrero" >
+        <img  src="fightOfTheMasters/img/${p2.name}.jpg">
+        <div class="name">${p2.name}</div>
+        <div class="health">${p2.health}</div>
+        <div class="strength">${p2.strength}</div>
+        <div class="defense">${p2.defense}</div>
+        <div class="agility">${p2.agility}</div>
+        <div class="stamina">${p2.stamina}</div>
+
+    </div>`;
+
 
 
     };
+
+    document.getElementById(playerTag).classList.add("selected");
+    document.getElementById(playerTag).onclick = "";
 };
 
 
@@ -68,6 +100,8 @@ let select = (character) => {
 
 let p1 = "";
 let p2 = "";
+let p3 = "";
+let p4 = "";
 
 //Seleccionar contrincantes funciona
 
@@ -75,25 +109,19 @@ let p2 = "";
 
 //funciones
 
-let fasingSwitch = (faseNow, Fasethen) => {
-    let chosingFase = document.getElementById(chosingFase);
+let fasingSwitch = (faseNow, faseThen) => {
+    let currentFase = document.getElementById(faseNow);
 
-    let fightingFase = document.getElementById(fightingFase);
+    let nextFase = document.getElementById(faseThen);
 
 
-    chosingFase.style.display = "none";
-    fightingFase.style.display = "block";
+    currentFase.style.display = "none";
+    nextFase.style.display = "block";
 };
 
 
-
-
-
-
-
-
 //traductor
-let allplayers = {
+let allPlayers = {
     "sonic": player1,
     "samus": player2,
     "mario": player3,
